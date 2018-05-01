978f76722675faa906abccda24f168719d7cdf51
James D. Ensor <je2062a@student.american.edu>
2018-05-01 15:36:12 -0400

    Seperate main df

--- R/percent_significant.R
+++ R/percent_significant.R
@@ -22,4 +22,41 @@
 
 percent_significant <- function(ts, df) {
   
+# Create list of columns in ts that contain non-date data.
+  num_of_col <- (2:ncol(stock))
+  
+  
+# Create data frames for each individual series. 
+  for (i in num_of_col) +
+    assign(paste("x", i, sep = ""), select(stock, 1, i))
+  
+  
+# List data frames.
+  xlist <- list(for (i in num_of_col){
+                  paste(", x", i, sep = "")
+    })
+                
 }
+
+for (i in num_of_col){
+  paste(", x", i, sep = "")
+  }
+
+
+lapply(list(df1, df2), function(w) { w$Avg <- rowMeans(w[1:2]); w })
+
+
+
+
+
+
+
+
+
+
+for (i in 2:ncol(stock)) assign(paste("x", i, sep = ""), select(stock, 1, i))
+  
+xlist <- list(x2, x3, x4, x5)
+
+lapply(xlist, 'filter', time > 50)
+
