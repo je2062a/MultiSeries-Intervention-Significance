5ddb28226359daf005287583f613a7bc41973e85
James D. Ensor <je2062a@student.american.edu>
2018-04-30 21:28:07 -0400

    Redefine function

--- R/percent_significant.R
+++ R/percent_significant.R
@@ -3,12 +3,11 @@
 #' Calculates the percentage of time-series that experienced significant change 
 #' due to an intervention.
 #'  
-#' This function provides simple analysis of multiple time-series data sets. The
-#' function can read time-series data that has already been fitted to an 
-#' "intervention at t = 0" format, or unformatted data by including a data set 
-#' that inlcudes the time of intervention. The output of this function
-#' represents the percent of individual time-series that experienced a 
-#' significant difference between pre-intervention and post-intervention data.
+#' This function provides simple analysis of multiple time-series data sets. 
+#' This function requires the inclusion of a data set that defines the time of
+#' intervention. The output of this function represents the percent of 
+#' individual time-series that experienced a significant difference between 
+#' pre-intervention and post-intervention data.
 #' 
 #'  @author James D. Ensor
 #'  
@@ -21,6 +20,6 @@
 #'  
 #'  @export
 
-percent_significant <- function(df1, df2, ) {
+percent_significant <- function(df) {
   
 }
