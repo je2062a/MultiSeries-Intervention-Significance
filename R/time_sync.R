bfb2a9810452abc12c188d85561bcb69934d90cc
James D. Ensor <je2062a@student.american.edu>
2018-05-02 01:23:32 -0400

    Start writing

--- R/time_sync.R
+++ R/time_sync.R
@@ -3,15 +3,21 @@
 #' Synchronize multiple interrupted time series so that intervention is 
 #' applied to all series at t = 0.
 #'  
-#' This function allows 
+#' This function provides the ability to synchronize multiple time-series with 
+#' varying times of 
 #' 
 #'  @author James D. Ensor
 #'  
-#'  @param ts A data frame containing multiple time-series.
-#'  @param df A data frame containing the time of intervention for each series.
+#'  @param x A data frame containing multiple time-series.
+#'  @param y A data frame containing the time of intervention for each series.
 #'  
 #'  @return 
 #'  
 #'  @examples 
 #'  
 #'  @export
+
+
+time_sync <- function(x, y) {
+  
+}
